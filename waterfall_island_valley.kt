845a956e085861815e7b2ed8621891cc7922b68c
antonenkoislucky <136689234+antonenkoislucky@users.noreply.github.com>
2023-06-20 17:16:24 +0700

    Add a new feature

---
+++ waterfall_island_valley.kt
@@ -0,0 +1,294 @@
+//1
+fun main() {
+    println("The Power of Possibility")
+}
+ 
+//2
+fun doSomething() {
+    println("Start exploring what is possible!")
+}
+
+//3
+val possibilities = listOf(
+    "Create something new",
+    "Achieve more from your effort",
+    "Have an impact on the world"
+)
+
+//4
+fun explorePossibility(possibility: String) {
+    println(possibility)
+}
+
+//5
+fun exploreAllPossibility() {
+    possibilities.forEach {
+        explorePossibility(it)
+    }
+}
+
+//6
+val myGoal = "My goal is to become an innovative problem solver"
+
+//7
+fun setGoals() {
+    println(myGoal)
+}
+
+//8
+val myAction = "I am going to come up with 3 ideas to solve this problem"
+
+//9
+fun actionSteps() {
+    println(myAction)
+}
+
+//10
+val myInvestment = "I will invest 10-15 hours a week to learn the skills needed to achieve my goal"
+
+//11
+fun investTime() {
+    println(myInvestment)
+}
+
+//12
+fun thinkOutOfBox(idea: String) {
+    println(idea)
+}
+
+//13
+val outOfBoxIdeas = listOf(
+    "Explore creativity",
+    "See things from a different perspective",
+    "Be open to different ideas and possibilities"
+)
+
+//14
+fun exploreOutOfBoxIdeas() {
+    outOfBoxIdeas.forEach {
+        thinkOutOfBox(it)
+    }
+}
+
+//15
+val myResult = "At the end of the day, I want to have a solution that works to solve the problem"
+
+//16
+fun setResult() {
+    println(myResult)
+}
+
+//17
+fun stayMotivated(motivation: String) {
+    println(motivation)
+}
+
+//18
+val motivations = listOf(
+    "Take the initiative to create change",
+    "Turn frustration into motivation",
+    "Create something bigger than yourself"
+)
+
+//19
+fun stayMotivatedAlways() {
+    motivations.forEach {
+        stayMotivated(it)
+    }
+}
+
+//20
+val myWisdom = "It's only when you make mistakes that you learn and grow"
+
+//21
+fun learnFromMistake() {
+    println(myWisdom)
+}
+
+//22
+fun reflectOnProgress(progress: String) {
+    println(progress)
+}
+
+//23
+val progressReflections = listOf(
+    "Track progress and celebrate successes",
+    "Re-evaluate plans and adjust as needed",
+    "Make adjustments to maintain momentum"
+)
+
+//24
+fun reflectOnProgressAlways() {
+    progressReflections.forEach {
+        reflectOnProgress(it)
+    }
+}
+
+//25
+val myThought = "If nothing changes, nothing will change"
+
+//26
+fun thinkMore() {
+    println(myThought)
+}
+
+//27
+fun takeAction(action: String) {
+    println(action)
+}
+
+//28
+val actions = listOf(
+    "Identify what needs to change",
+    "Take control of your journey",
+    "Take one step forward"
+)
+
+//29
+fun takeActionAlways() {
+    actions.forEach {
+        takeAction(it)
+    }
+}
+
+//30
+val myIdeas = "I will ask myself 'What if I do this?' and see where the possibilities take me"
+
+//31
+fun generateIdeas() {
+    println(myIdeas)
+}
+
+//32
+fun overcomeChallenges(overcomeStrategy: String) {
+    println(overcomeStrategy)
+}
+
+//33
+val overcomeStrategies = listOf(
+    "Identify obstacles and plan how to overcome them",
+    "Ask for help when needed",
+    "Take risks and think outside of the box"
+)
+
+//34
+fun overcomeChallengesAlways() {
+    overcomeStrategies.forEach {
+        overcomeChallenges(it)
+    }
+}
+
+//35
+val myVision = "My vision is to create something new and inspiring that has the power to make an impact"
+
+//36
+fun paintThePicture() {
+    println(myVision)
+}
+
+//37
+fun keepMotivated(motivation: String) {
+    println(motivation)
+}
+
+//38
+val motivations2 = listOf(
+    "See the bigger picture and the potential",
+    "Surround yourself with inspiring people",
+    "Focus on the progress and keep improving"
+)
+
+//39
+fun keepMotivatedAlways() {
+    motivations2.forEach {
+        keepMotivated(it)
+    }
+}
+
+//40
+val myMindset = "Having a growth mindset will help me overcome any challenge, obstacle, or setback that I face"
+
+//41
+fun haveGrowthMindset() {
+    println(myMindset)
+}
+
+//42
+fun believeInMyself(belief: String) {
+    println(belief)
+}
+
+//43
+val beliefs = listOf(
+    "Know that failure is not the end, but just a learning experience",
+    "Be confident and believe in your ability to make a change",
+    "Take on any challenge with an open mind"
+)
+
+//44
+fun believeInMyselfAlways() {
+    beliefs.forEach {
+        believeInMyself(it)
+    }
+}
+
+//45
+val myDetermination = "I will be determined and resilient throughout my journey and never give up"
+
+//46
+fun stayDetermined() {
+    println(myDetermination)
+}
+
+//47
+fun shareMyStory(story: String) {
+    println(story)
+}
+
+//48
+val stories = listOf(
+    "Share your experience and what you have learned",
+    "Inspire others to pursue their passions",
+    "Encourage others to take control of their journey"
+)
+
+//49
+fun shareMyStoryAlways() {
+    stories.forEach { 
+        shareMyStory(it)
+    }
+}
+
+//50
+val myConclusion = "As I continue on my journey, I will never forget the power of possibility"
+
+//51
+fun neverForget() {
+    println(myConclusion)
+}
+
+//52
+fun main() {
+    println("The Power of Possibility")
+    doSomething()
+    exploreAllPossibility()
+    setGoals()
+    actionSteps()
+    investTime()
+    exploreOutOfBoxIdeas()
+    setResult()
+    stayMotivatedAlways()
+    learnFromMistake()
+    reflectOnProgressAlways()
+    thinkMore()
+    takeActionAlways()
+    generateIdeas()
+    overcomeChallengesAlways()
+    paintThePicture()
+    keepMotivatedAlways()
+    haveGrowthMindset()
+    believeInMyselfAlways()
+    stayDetermined()
+    shareMyStoryAlways()
+    neverForget()
+}
