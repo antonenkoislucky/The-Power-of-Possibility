01b1d90720813db7f0569817c2f655623c19bcb7
antonenkoislucky <136689234+antonenkoislucky@users.noreply.github.com>
2023-07-19 18:10:18 +0700

    Add error handling for edge cases

---
+++ xylophone_cat_king.py
@@ -0,0 +1,66 @@
+#The Power of Possibility
+
+#This program will show the power of possibility through examples. 
+
+#Example 1
+#The power of possibility enables us to think in new ways and explore previously unknown solutions. 
+
+#Defining some initial variables
+x = 0
+y = 0
+
+#Creating a loop to iterate over
+for i in range(100):
+	#Calculating a new value of x and y
+	x = x + y
+	y = y + 3
+	#Printing out the current ratio of x to y
+	print("x/y =", x/y)
+
+#Example 2
+#The power of possibility helps us to unlock potential in new areas of life. 
+
+#Defining some initial variables 
+n = 0
+m = 0
+
+#Creating a loop to iterate over 
+for i in range(100): 
+	#Calculating a new value of n and m 
+	n = n + m
+	m = m + 10
+	#Printing out the current ratio of n to m 
+	print("n/m =", n/m)
+
+#Example 3
+#The power of possibility helps us to create creativity where otherwise there was none.
+
+#Defining some initial variables 
+a = 0
+b = 0
+
+#Creating a loop to iterate over 
+for i in range(100): 
+	#Calculating a new value of a and b 
+	a = a + b
+	b = b + 5
+	#Printing out the current ratio of a to b 
+	print("a/b =", a/b)
+
+#Example 4
+#The power of possibility allows us to break through the boundaries of what is conventionally thought possible. 
+
+#Defining some initial variables 
+p = 0
+q = 0
+
+#Creating a loop to iterate over 
+for i in range(100): 
+	#Calculating a new value of p and q 
+	p = p + q
+	q = q + 2
+	#Printing out the current ratio of p to q 
+	print("p/q =", p/q)
+
+#Conclusion
+#The power of possibility is truly limitless. Through creative thinking, exploration, and the courage to break through boundaries, anything is possible.
